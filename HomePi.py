37465fcb448ee6779325dea670f2d8fd1373c990
Frank E. Hernandez <hernandez.frank@gmail.com>
2019-05-06 20:06:39 -0400

    Replace config client/server BT MAC addresses to reduce user configration steps

--- HomePi.py
+++ HomePi.py
@@ -214,17 +214,24 @@ class HomePiManager(object):
 		# Get This PiHome Id
 		self.homePiId = json_data['homePiId']
 
+		# Replace Mac Addresses with system addresses. 
+		# Note: Users will no longer have to specify these values 
+		# because will get them from the system
+		macs = self.getHomePiBluetoothInterfaces()
+		json_data[self.KEY_CLIENT_INTERFACE_MAC] = macs[0]
+		json_data[self.KEY_SERVER_INTERFACE_MAC] = macs[1]
+		
 		# Get the Mac for the interface to use to connect 
 		# with registered devices.
-		self.clientInterfaceMac = json_data['clientInterfaceMac']
+		self.clientInterfaceMac = json_data[self.KEY_CLIENT_INTERFACE_MAC]
 			
 		# Get the Mac for the interface to use to listen for
 		# clients.
-		self.serverInterfaceMac = json_data['serverInterfaceMac']
+		self.serverInterfaceMac = json_data[self.KEY_SERVER_INTERFACE_MAC]
 
 
 		# Get devices in JSON config
-		json_devs = json_data['devices']
+		json_devs = json_data[self.KEY_DEVICES]
 	
 		# List of registered devices in the HomePi
 		self.registeredDevices = []
